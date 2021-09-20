e7c7fb507adae3a17767bdb5e34f435cc50f649b
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2021-09-20 20:00:24 +0800

    Update FSub.py

--- FSub.py
+++ FSub.py
@@ -18,6 +18,11 @@ app = Client(
       bot_token = Config.BOT_TOKEN,
       api_id = Config.APP_ID,
       api_hash = Config.API_HASH,
+      owner_username = config.OWNER_USERNAME,
+      updates_ch = Config.UPDATES_CH,
+      support_grp = Config.SUPPORT_GRP,
+      updates_name = config.UPDATES_NAME,
+      support_name = config.SUPPORT_NAME
       plugins = plugins
 )
 
