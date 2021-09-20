9c5f4f9c3c1bb31fd546a2b499adb2e93b5b418b
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2021-09-20 19:57:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ class Config(object):
 	OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
 	UPDATES_CH = os.environ.get("UPDATES_CH")
 	SUPPORT_GRP = os.environ.get("SUPPORT_GRP")
+	UPDATES_NAME = os.environ.get("UPDATES_NAME")
+	SUPPORT_NAME = os.environ.get("SUPPORT_NAME")
 	APP_ID = int(os.environ.get("APP_ID"))
 	API_HASH = os.environ.get("API_HASH")
 	DATABASE_URL = os.environ.get("DATABASE_URL")
