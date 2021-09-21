91b811db4fb6fcad4bec9d53a502a66de3f7225b
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2021-09-21 09:53:21 +0800

    Update FSub.py

--- FSub.py
+++ FSub.py
@@ -7,20 +7,23 @@ logging.basicConfig(level=logging.INFO)
 plugins = dict(
     root="plugins",
     include=[
-        "forceSubscribe",
+        "FSub-TeleBot-ENG",
         "help"
     ]
 )
 
 app = Client(
-     'ForceSubscribe',
+     'FSub-TeleBot-ENG',
       bot_token = Config.BOT_TOKEN,
+      bot_name = Config.BOT_NAME,
+      bot_username = Config.BOT_USERNAME,
       api_id = Config.APP_ID,
       api_hash = Config.API_HASH,
       updates_ch = Config.UPDATES_CH,
       support_grp = Config.SUPPORT_GRP,
       updates_name = Config.UPDATES_NAME,
       support_name = Config.SUPPORT_NAME,
+      owner_username = Config.OWNER_USERNAME
       plugins = plugins
 )
 
