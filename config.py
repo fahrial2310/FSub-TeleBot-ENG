e4f7d1f500acbbc1786550a10459809a7f199b7b
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2021-09-21 17:29:10 +0800

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class Messages():
       HELP_MSG = [
         ".",
 
-        f"[☠️](https://telegra.ph/file/ac48f2037d79b1763d089.jpg) **FORCE SUBSCRIBE :**\n\nhey,i'm {BOT_NAME} can Force Group Members To Join A Specific Channel Before Sending Messages in The Group.\nI Will Mute Members if They Not Joined Your Channel And Tell Them To Join The Channel And Unmute Themself By Pressing A Button.",
+        "[☠️](https://telegra.ph/file/ac48f2037d79b1763d089.jpg) **FORCE SUBSCRIBE :**\n\nhey there,i can Force Group Members To Join A Specific Channel Before Sending Messages in The Group.\nI Will Mute Members if They Not Joined Your Channel And Tell Them To Join The Channel And Unmute Themself By Pressing A Button.",
         
         "[☠️](https://telegra.ph/file/83c970d7130ead36a8b72.jpg) **SETUP :**\n\nFirst Of All Add Me In The Group As Admin With Ban Users Permission And In The Channel As Admin.\n☠️ Note: Only Creator Of The Group Can Setup Me.",
         
