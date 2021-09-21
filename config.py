8e32b0d55e8ed84afee34eec8896847dc8accd99
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-09-21 15:38:53 +0700

    lol miss f string

--- config.py
+++ config.py
@@ -20,13 +20,13 @@ class Messages():
       HELP_MSG = [
         ".",
 
-        "[☠️](https://telegra.ph/file/ac48f2037d79b1763d089.jpg) **FORCE SUBSCRIBE :**\n\nhey,i'm {BOT_NAME} can Force Group Members To Join A Specific Channel Before Sending Messages in The Group.\nI Will Mute Members if They Not Joined Your Channel And Tell Them To Join The Channel And Unmute Themself By Pressing A Button.",
+        f"[☠️](https://telegra.ph/file/ac48f2037d79b1763d089.jpg) **FORCE SUBSCRIBE :**\n\nhey,i'm {BOT_NAME} can Force Group Members To Join A Specific Channel Before Sending Messages in The Group.\nI Will Mute Members if They Not Joined Your Channel And Tell Them To Join The Channel And Unmute Themself By Pressing A Button.",
         
         "[☠️](https://telegra.ph/file/83c970d7130ead36a8b72.jpg) **SETUP :**\n\nFirst Of All Add Me In The Group As Admin With Ban Users Permission And In The Channel As Admin.\n☠️ Note: Only Creator Of The Group Can Setup Me.",
         
         "[☠️](https://telegra.ph/file/fe1111b216042f2cfb883.jpg) **COMMMANDS :**\n\n/ForceSubscribe - To Get The Current Settings.\n/ForceSubscribe no/off/disable - To Turn Of ForceSubscribe.\n/ForceSubscribe {Channel Username} - To Turn On And Setup The Channel.\n/ForceSubscribe clear - To Unmute All Members Who Muted By Me.\n\n● Note: /FSub Is An Alias Of /ForceSubscribe",
         
-        "[☠️](https://telegra.ph/file/ac48f2037d79b1763d089.jpg) **DEVELOPED BY {OWNER_USERNAME}**"
+        f"[☠️](https://telegra.ph/file/ac48f2037d79b1763d089.jpg) **DEVELOPED BY {OWNER_USERNAME}**"
       ]
 
-      START_MSG = "**Hey! [☠️](https://telegra.ph/file/ac48f2037d79b1763d089.jpg) [{}](tg://user?id={})**\n\nmy name is {BOT_NAME},I Can Force Members To Join A Specific Channel Before Writing Messages In The Group.\n● Learn More At 👉 /help"
+      START_MSG = f"**Hey! [☠️](https://telegra.ph/file/ac48f2037d79b1763d089.jpg) [{}](tg://user?id={})**\n\nmy name is {BOT_NAME},I Can Force Members To Join A Specific Channel Before Writing Messages In The Group.\n● Learn More At 👉 /help"
